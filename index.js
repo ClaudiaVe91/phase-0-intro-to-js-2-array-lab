478640502db12f4d9b79f38f82f927844c3063f6
claudia <claudiavaldez1991@gmail.com>
2022-12-21 11:01:23 -0400

    commit tree dadcf9f59e3ba5b6f3c3dbf4fbf6b24d4b4c816b

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(Ralph){
+    cats.push(Ralph)
+}
+function destructivelyPrependCat(Bob){
+    cats.unshift(Bob)
+}
+function destructivelyRemoveLastCat(Ralph){
+    cats.pop()
+}
+function destructivelyRemoveFirstCat(Milo){
+    cats.shift()
+}
+function appendCat(Broom){
+    const copyOfCats = [...cats]
+    const newCats = [...cats, "Broom"]
+    return(newCats)
+}
+
+function prependCat(Arnold){
+    const copyOfCats = [...cats]
+    const newCats = ["Arnold", ...cats]
+    return(newCats)
+}
+
+function removeLastCat(Garfield){
+    const copyOfCats = [...cats]
+    copyOfCats.pop();
+    return(copyOfCats)
+}
+
+function removeFirstCat(Milo){
+    const copyOfCats = [...cats]
+    copyOfCats.shift();
+    return(copyOfCats)
+}
+
+    
+
+
+        
